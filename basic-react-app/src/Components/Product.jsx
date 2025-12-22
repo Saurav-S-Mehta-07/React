5f8d0828daf162d21ff2f460407c3f8d602f85bf
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-22 13:14:42 +0530

    condition in jsx

--- basic-react-app/src/Components/Product.jsx
+++ basic-react-app/src/Components/Product.jsx
@@ -8,9 +8,8 @@ function Product({title,description,price=700,features,option}){
           <Title title={title}/>
           <Description description={description}/>
           <h3>Price : {price}</h3>
-          
+          {price>700?<p>Discout of 5%</p>:null}
           <ul>{features.map(feature=><li>{feature}</li>)}</ul>
-        
           <p>{option.a}</p>
         </div>
     )
