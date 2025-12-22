ff9f50cd44a4decdf7ccbe9d88c58d3ef316855d
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-22 13:09:25 +0530

    rendering array and object

--- basic-react-app/src/Components/Product.jsx
+++ basic-react-app/src/Components/Product.jsx
@@ -2,12 +2,16 @@ import Title from "./Title";
 import Description from "./Description";
 import "../ComponentsCss/Product.css"
 
-function Product({title,description,price}){
+function Product({title,description,price=700,features,option}){
     return (
         <div className="Product">
           <Title title={title}/>
           <Description description={description}/>
           <h3>Price : {price}</h3>
+          
+          <ul>{features.map(feature=><li>{feature}</li>)}</ul>
+        
+          <p>{option.a}</p>
         </div>
     )
 }
