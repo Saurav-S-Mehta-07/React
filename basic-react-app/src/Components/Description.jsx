8b03d98756b66f358360f3ff27760b54724c0ae4
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-22 11:55:00 +0530

    1st component

---
+++ basic-react-app/src/Components/Description.jsx
@@ -0,0 +1,12 @@
+import "../ComponentsCss/Description.css";
+
+function Description(){
+    return (
+        <>
+           <p>this is a description</p>
+           <p>returning more tags</p>
+        </>
+    )
+}
+
+export default Description;
---
+++ basic-react-app/src/Components/Title.jsx
@@ -0,0 +1,7 @@
+import "../ComponentsCss/Title.css"
+
+function Title(){
+    return <h2>I'm Title</h2>
+}
+
+export default Title;
